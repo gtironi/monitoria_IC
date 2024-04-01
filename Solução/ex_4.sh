0cd1a6fb43b9ba85f877c0acf3e78e143b3c00ed
Gustavo Tironi <126670916+gtironi@users.noreply.github.com>
2024-04-01 13:49:09 +0000

    Arrumei os exercicios 4, 5 e 6

--- Solução/ex_4.sh
+++ Solução/ex_4.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sed 's/ /\n/g' ../dados/machado/contos/macn001.txt | sed '/^$/d' | tr -d '[!"#$%&()*+,\./:;<=>?@[\]^_`{|}~]' | LC_ALL='C' sort -bdf  | uniq -i -c | LC_ALL='C' sort -rn -k 1 > resposta_ex_4.txt
+sed 's/ /\n/g' ../dados/machado/contos/macn001.txt | sed '/^[[:space:]]*$/d' | tr -d '[!"#$%&()*+,\./:;<=>?@[\]^_`{|}~]' | LC_ALL='C' sort -bdf  | uniq -i -c | LC_ALL='C' sort -rn -k 1 > resposta_ex_4.txt
